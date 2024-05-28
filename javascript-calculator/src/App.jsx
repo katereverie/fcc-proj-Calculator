8db1dd514971329e524f2f923e920c48d6393248
Katereverie <143613315+Katereverie@users.noreply.github.com>
2024-05-28 16:57:06 +0200

    passed all tests. code still buggy

--- javascript-calculator/src/App.jsx
+++ javascript-calculator/src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react';
 import './App.css';
 
-
-let result = null;
-
 function App() {
     
     const [display, setDisplay] = useState('0');
     const [expression, setExpression] = useState('');
-
-    const operators = ['÷', '×', '-', '+'];
+    const [result, setResult] = useState('');
+    
+    const operators = ['÷', '×', '−', '+'];
     const numberRegex = /(\d+(\.\d+)?)/g;
-    const operatorRegex = /(\d+(\.\d+)?|[+-÷×])/g;
+    const operatorRegex = /(\d+(\.\d+)?|[+−÷×])/g;
 
     const operate = (a, b, operator) => {
+
+        console.log(`operator is: ${operator}`);
+        console.log(`the values are ${a} and ${b}`);
         switch (operator) {
             case '+':
                 return a + b;
-            case '-':
+            case '−':
                 return a - b;
             case '×':
                 return a * b;
@@ -29,7 +30,7 @@ function App() {
             default:
                 throw new Error("something is off");
         }
-    }
+    }    
     
     const handleClickedInput = (e) => {
 
@@ -37,17 +38,17 @@ function App() {
 
         // since the programmers do not know if the users input a digit or a decimal point,
         // it is necessary for the programmers to check it.
-        const isValueOperator = operators.includes(value) ? true : false;
+        const isValueOperator = operators.includes(value);
         const isValueDecimal = value === '.' ? true: false;
         const isValueZero = value === '0'? true: false;
-        
+        const isResultDisplayed = expression.includes('=');
+        const isValueMinus = value === '−';
 
         // if operators are clicked, preserve result for expression; replace result with input value
         // if a number input (including decimal) is clicked, clear result for both display and expression
-
         if (!isValueOperator) {
-            // input value is not an operator (but a digit [0-9] or a decimal point ".")
-            isValueDecimal? 
+            isValueDecimal?
+            // input is a decimal point 
             (
                 setDisplay(prevDisplay => {
                     const hasDecimal = prevDisplay.includes('.') ? true: false;
@@ -70,11 +71,6 @@ function App() {
                     const hasDecimalNumber = numberToken.some(num => num.includes('.'));
                     const isLastNumberDecimalNumber = lastNumber.includes('.');
 
-                    // console.log("number token:", numberToken);
-                    // console.log("last number", lastNumber);
-                    // console.log("hasDecimalNumber", hasDecimalNumber);
-                    // console.log("is last number decimal number", isLastNumberDecimalNumber);
-
                     if (prevExpression[prevExpression.length - 1] === '.') return prevExpression;
                     if (hasDecimalNumber && isLastNumberDecimalNumber) return prevExpression;
                     
@@ -82,12 +78,13 @@ function App() {
                 })
             )
             :
+            // input is between [0-9]
             (
                 setDisplay(prevDisplay => {
-                
-                    const isPrevDisplayOperator = operators.includes(prevDisplay)? true: false;
 
-                    if (isPrevDisplayOperator || (prevDisplay == '0' && !isValueZero)) {
+                    const isPrevDisplayOperator = operators.includes(prevDisplay);
+
+                    if (isPrevDisplayOperator || (prevDisplay == '0' && !isValueZero) || isResultDisplayed) {
                         return value;
                     } else if (prevDisplay == '0' && isValueZero) {
                         return '0';
@@ -96,23 +93,32 @@ function App() {
                     }
                 }),
                 setExpression(prevExpression => {
-                    if (prevExpression === '' || prevExpression === '0') return value;
+                    if (prevExpression === '' || prevExpression === '0' || isResultDisplayed) return value;
                     return prevExpression.concat(value);
                 })
             );
 
         } else {
             // input value is an operator
+
+            const lastChar = expression[expression.length - 1];
+            const secondLastChar= expression[expression.length - 2];
+            const isLastCharOperator = operators.includes(lastChar);
+            const areLastTwoCharsOperators = operators.includes(lastChar) && operators.includes(secondLastChar);
+
+            if (isResultDisplayed) {
+                setExpression(result + value);
+            }
+            
+            // no result displayed
             setDisplay(value);
             setExpression(prevExpression => {
 
-                const lastChar = prevExpression[prevExpression.length - 1];
-                const isLastCharOperator = operators.includes(lastChar)? true : false;
-            
-
                 if (prevExpression === '') return value;
-                if (isLastCharOperator && lastChar === value ) return prevExpression;
+                if (isLastCharOperator && isValueMinus && !areLastTwoCharsOperators) return prevExpression.concat(value);
+                if (isLastCharOperator && lastChar === value) return prevExpression;
                 if (isLastCharOperator && lastChar !== value) return prevExpression.slice(0, -1).concat(value);
+                
                 return prevExpression.concat(value);
             });
         }
@@ -121,48 +127,50 @@ function App() {
     
     // when users click 'equals', calculate and display results
     const handleCalculation = () => {
+
+        const tokens = expression.match(operatorRegex);
+        const hasOperatorAtEnd = operators.includes(tokens[tokens.length - 1]);
+
+        // if the last element of tokens is an operator, remove it
+        console.log("matched tokens: ", tokens);
+        if (hasOperatorAtEnd) console.log("tokens without =: ", tokens.pop);
         
-        setExpression(prevExpression => {
-    
-            
-            const expressionArr = prevExpression.match(operatorRegex);
-            console.log("expression array after tokenization: ", expressionArr);
-            const hasOperatorAtEnd = operators.includes(expressionArr[expressionArr.length - 1]) ? true: false;
-            
-            // if the last element of expressionArr is an operator, remove it
-            if (hasOperatorAtEnd) expressionArr.pop();
 
-            // convert number strings to numbers 
-            const numbers = expressionArr.map(element => isNaN(element) ? element: parseFloat(element));
+        // convert number strings to numbers 
+        const convertedTokens = tokens.map(token => isNaN(token) ? token: Number(token)
+        );
 
-            result = numbers[0];
-            let currentOperator = null;
+        console.log(`maped tokens: ${convertedTokens}`);
 
-            for (let i = 1; i < numbers.length; i++) {
-                const element = numbers[i];
-
-                if (typeof element === 'string') {
-                    currentOperator = element;
-                } else {
-                    result = operate(result, element, currentOperator);
-                }
-            }
-
-            return prevExpression.concat(`=${result}`);
-        });
+        let currentResult = convertedTokens[0];
+        console.log(`the currentResult is: ${currentResult}` );
+        let currentOperator = null;
         
-        setDisplay(prevDisplay => {
-            if (result !== null) {
-                prevDisplay = result;
+        for (let i = 1; i < convertedTokens.length; i++) {
+            const currentToken = convertedTokens[i];
+            const nextToken = convertedTokens[i + 1];
+
+            if (typeof currentToken === 'string' & nextToken !== '−') {
+                currentOperator = currentToken;
+            } else if (typeof currentToken === 'string' && nextToken === '−') {
+                currentOperator = currentToken;
+                convertedTokens[i + 2] = -convertedTokens[i + 2];
+                convertedTokens.splice(i + 1, 1);
+            } else {
+                currentResult = operate(currentResult, currentToken, currentOperator);
             }
-            return prevDisplay;
-        });    
+        }
+
+        setResult(currentResult);
+        setExpression(prevExpression => prevExpression.concat(`=${currentResult}`));
+        setDisplay(String(currentResult));
+
     }
 
     const clear = () => {
         setDisplay('0');
         setExpression('');
-        result = null;
+        setResult('');
     }
 
     return (
@@ -198,7 +206,7 @@ function App() {
                         <button id='four' className='number-btn' value='4' onClick={handleClickedInput}>4</button>
                         <button id='five' className='number-btn' value='5' onClick={handleClickedInput}>5</button>
                         <button id='six' className='number-btn' value='6' onClick={handleClickedInput}>6</button>
-                        <button id='subtract' className='operator' value='-' onClick={handleClickedInput}>-</button>
+                        <button id='subtract' className='operator' value='−' onClick={handleClickedInput}>−</button>
                     </div>
                     <div className='button-wrapper'>
                         <button id='one' className='number-btn' value='1' onClick={handleClickedInput}>1</button>
